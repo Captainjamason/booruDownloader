a0595706e66da1114a9504e357b6652e53d2e690
Jamason Davis <jpdavis2471@gmail.com>
2023-12-27 18:34:34 -0600

    Semi-Functional Image Downloading?

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use pgbar::{ProgressBar, Style};
+use anyhow::{Result, Ok, Error};
+use reqwest::Response;
+use std::{fs::File, io::{copy, Cursor}};
 
-fn main() {
-    progressBar("test".to_string());
+#[tokio::main]
+async fn main() -> Result<()> {
+    match downloadImage().await {
+        std::result::Result::Ok(_) => println!("image downloaded"),
+        Err(e) => println!("err"),
+    }
+    Ok(())
+}
+
+async fn downloadImage()-> Result<()>{
+    let response = reqwest::get("https://testbooru-cdn.donmai.us/original/3a/0e/3a0e769991323dcf9748adba9ab530dc.jpgjpg").await?;
+    let mut file = File::create("./test.jpg")?;
+    let mut content = Cursor::new(response.bytes().await?);
+    copy(&mut content, &mut file)?;
+
+    Ok(())
 }
 
 fn progressBar(name: String) {
