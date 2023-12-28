9d76d9f01be0bef2219f0219d66b89c4ab8813e9
Jamason Davis <jpdavis2471@gmail.com>
2023-12-28 16:33:45 -0600

    Download into images folder. boutta start reorganizing into sane files

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 size_t writeImage(char *ptr, size_t size, size_t nmemb, void *userdata) {
     size_t realsize = size * nmemb;
     std::ofstream file;
-    file.open("test.json");
+    file.open("images/test.jpg");
     file.write(ptr, realsize);
     file.close();
     return 0;
@@ -21,7 +21,7 @@ size_t writeImage(char *ptr, size_t size, size_t nmemb, void *userdata) {
 int downloadImage() {
     std::cout << "Downloading Image";
     CURL *easy = curl_easy_init(); // Init the easy handle for syncro based downloading, maybe add async support in the future?
-    curl_easy_setopt(easy, CURLOPT_URL, "https://testbooru.donmai.us/posts/9863.json"); // URL to download
+    curl_easy_setopt(easy, CURLOPT_URL, "https://testbooru-cdn.donmai.us/original/3a/0e/3a0e769991323dcf9748adba9ab530dc.jpg"); // URL to download
     curl_easy_setopt(easy, CURLOPT_USERAGENT, "libcurl-agent/1.0"); // User agent, required for danbooru
     curl_easy_setopt(easy, CURLOPT_WRITEFUNCTION, writeImage); // Callback to write image
     //curl_easy_setopt(easy, CURLOPT_USERPWD, "Captainjamason:mU4ba6jikixpkNhiDDbTPnHL");   // Authenticate the user
