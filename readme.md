fabda47026d98751aa15127846961c7a629f23ee
Jamason Davis <jpdavis2471@gmail.com>
2023-12-28 12:28:36 -0600

    Readme, Git ignore, Somewhat working post info download.

---
+++ readme.md
@@ -0,0 +1,24 @@
+# booruDownloader
+
+A lightweight cross-platform C++ program for downloading images in bulk from imageboard sites.
+
+### Planned Features:
+
+* [ ] CLI Interface.
+* [ ] Fledged Build System with `cmake`
+* [ ] Full Danbooru support.
+* [ ] Gelbooru support.
+* [ ] Basic GUI done in `wxwidgets`
+* [ ] Multithreading
+* [ ] Sorting by `tag` or `character` into folders
+* [ ] Customizable config
+
+
+### Requirements
+
+* `libcurl=<8.5.0`
+
+
+<3
+
+Copywrite 2023 - JPD/hurbIndustries
--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,28 @@
 #include <iostream>
 #include <curl/curl.h>
 #include <curl/easy.h>
+#include <fstream>
+
+size_t writeImage(char *ptr, size_t size, size_t nmemb, void *userdata) {
+    size_t realsize = size * nmemb;
+    std::ofstream file;
+    file.open("test.json");
+    file.write(ptr, realsize);
+    file.close();
+    return 0;
+}
 
 // Image Download Function
 int downloadImage() {
-    CURL *easy = curl_easy_init();
-    curl_easy_setopt(easy, CURLOPT_URL, "https://testbooru.donmai.us/posts/9863");
-    curl_easy_perform(easy);
-    curl_easy_cleanup(easy);
-    return 1;
+    std::cout << "Downloading Image";
+    CURL *easy = curl_easy_init(); // Init the easy handle for syncro based downloading, maybe add async support in the future?
+    curl_easy_setopt(easy, CURLOPT_URL, "https://testbooru.donmai.us/posts/9863.json"); // URL to download
+    curl_easy_setopt(easy, CURLOPT_USERAGENT, "libcurl-agent/1.0"); // User agent, required for danbooru
+    curl_easy_setopt(easy, CURLOPT_WRITEFUNCTION, writeImage); // Callback to write image
+    //curl_easy_setopt(easy, CURLOPT_USERPWD, "Captainjamason:mU4ba6jikixpkNhiDDbTPnHL");   // Authenticate the user
+    curl_easy_perform(easy); // do the do
+    curl_easy_cleanup(easy); // clean up
+    return 0;
 }
 
 int main(int argc, char *argv[])
