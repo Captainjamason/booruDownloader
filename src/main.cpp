e84e80e6bae63c527f1315316122b910f679e5ac
Jamason Davis <jpdavis2471@gmail.com>
2023-12-28 12:11:42 -0600

    Successfully download with curl

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
+//      booruDownloader 
+//      Jamason P Davis
+//      Copywrite 2023
+//            <3
+
 #include <iostream>
+#include <curl/curl.h>
+#include <curl/easy.h>
+
+// Image Download Function
+int downloadImage() {
+    CURL *easy = curl_easy_init();
+    curl_easy_setopt(easy, CURLOPT_URL, "https://testbooru.donmai.us/posts/9863");
+    curl_easy_perform(easy);
+    curl_easy_cleanup(easy);
+    return 1;
+}
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+    downloadImage();
 }
