92853ace243a7d3482d9cc6f719ca6debb737fa8
Jamason Davis <jpdavis2471@gmail.com>
2024-01-25 09:09:44 -0600

    Updated install.sh to be more user friendly. Working under freebsd, Unknown on other OS's

--- install.sh
+++ install.sh
@@ -1,15 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #	boorudownloader
 #	install.sh
 #	Jamason P Davis
 #	Copyright 2024.
 
-if [[ $OSTYPE == freebsd* ]]; then
-	echo "OS: FreeBSD, setting CC to clang17..."
-	CC=clang17 meson setup build && cd build
-	meson compile
-	echo "Installing boorudownloader to /usr/local/bin/, Please provide elevation..."
-	sudo cp ./src/boorudownloader /usr/local/bin/boorudownloader
+if [[ $1 == "--clean-build" ]]; then
+	echo "Cleaning build directory..."
+	rm -rf ./build/
+	echo "Done! Proceeding to build..."
 fi
 
+if [[ $1 == "--clean" ]]; then
+	echo "Cleaning build directory..."
+	rm -rf ./build/
+	echo "Done!"
+	exit
+fi
+
+if [[ $OSTYPE == freebsd* ]]; then
+	echo "OS: FreeBSD, setting CC to clang17..."
+	export CC=clang17 
+fi
+
+if ! [ -d ./build/ ]; then
+	echo "Build directory not found, Setting up now..."
+	meson setup build && cd ./build
+else 
+	echo "Build directory found, Starting compilation..."
+	cd ./build
+fi
+
+meson compile
+
+if [[  $1 == "--only-build" ]];  then
+	echo "All done."
+else
+	echo "Installing the /usr/local/bin/, Requiring elevation to install."
+	sudo cp ./src/boorudownloader /usr/local/bin/boorudownloader
+	echo "Installed!"
+fi
